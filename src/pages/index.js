f5c4f07b7887ad01e5bd4df079b740b1949b1ef1
Freddy Polania <fpolania@gruponovanet.com.ve>
2019-05-31 07:40:21 -0400

    Index template
    Page and components rebuild

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,49 @@
 import React from "react"
 import Header from "../components/header"
 import Navigation from "../components/navigation"
-import Links from "../components/links"
-import Footer from "../components/footer"
 import containerStyles from "./index.module.css"
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
-library.add(fab)
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const IndexPage = () => (
+library.add(fab)
+let faicon = null
+const IndexPage = ({ data }) => (
 
     <main>
       <div className={containerStyles.menu}>
         <Header />
         <Navigation />
       </div>
-      <Links />
-      <Footer />
+      <footer>
+        <div className={containerStyles.links}>
+          {data.allStrapiLink.edges.map(document => (
+            <div key={document.node.id}>
+              <a href={document.node.URL} rel="noopener noreferrer" target="_blank">
+                <FontAwesomeIcon icon={['fab', faicon = document.node.icon.replace(/'/g,'')]} size="lg" />
+              </a>
+            </div>
+          ))}
+        </div>
+        <p>flaex.design ® Freddy Polanía {new Date().getFullYear()}</p>
+      </footer>
     </main>
 
 )
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allStrapiLink {
+      edges {
+        node {
+          id
+          icon
+          URL
+        }
+      }
+    }
+  }
+`
