ee281a6251d6c38c790741cacc4ebe35cfe9a05c
Freddy Polania <fpolania@gruponovanet.com.ve>
2019-06-09 22:55:56 -0400

    Desing page creation

---
+++ src/pages/design.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { Link, StaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
+/** @jsx jsx */
+import {css,jsx} from '@emotion/core'
+import Layout from '../components/layout'
+import containerStyles from "../pages/blog.module.less"
+
+const DesignTemplate = ({ data }) => (
+  <Layout>
+  <div className={containerStyles.navsec}>
+    <div>
+      <Link to="/bio">bio</Link>
+    </div>
+    <div
+      css={css `
+        text-decoration: underline !important;
+        -webkit-filter: invert(1);
+        filter: invert(1);
+      `}
+    >
+    <Link to="/blog">blog</Link>
+    </div>
+  </div>
+    <StaticQuery
+      query={graphql`
+        query DesignTemplate {
+          allStrapiArticle {
+            edges {
+              node {
+                id
+               title
+               description
+               date (
+                formatString: "DD MMMM YYYY"
+                locale: "en-US"
+               )
+               image {
+                  childImageSharp {
+                    fluid(maxWidth: 600) {
+                     ...GatsbyImageSharpFluid
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      `}
+    render={data => (
+        <ul className={containerStyles.articles}>
+          {data.allStrapiArticle.edges.map(document => (
+            <li key={document.node.id}>
+              <Link to={`/${document.node.id}`}>
+                <Img fluid={document.node.image.childImageSharp.fluid} />
+              </Link>
+              <time>{document.node.date}</time>
+              <h2>
+                <Link to={`/${document.node.id}`}>{document.node.title}</Link>
+              </h2>
+            </li>
+          ))}
+        </ul>
+    )}/>
+  </Layout>
+
+)
+
+export default DesignTemplate
