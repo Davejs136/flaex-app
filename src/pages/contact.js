9fbc9a35b8fdbc5e7867401514ae4f7e1468d3aa
Freddy Polania <fpolania@gruponovanet.com.ve>
2019-06-14 21:16:20 -0400

    Contact page
    - Contact form  integration with emailJS
    **FORM NEEDS VALIDATION**

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,61 +1,31 @@
 import React from 'react'
 import Layout from '../components/layout'
-import containerStyles from "../pages/bio.module.less"
+import * as emailjs from 'emailjs-com';
 
 export default class IndexPage extends React.Component {
-  state = {
-    fullName: "",
-    email: "",
-    message:""
-  }
-
-  handleInputChange = event => {
-    const target = event.target
-    const value = target.value
-    const name = target.name
-
-    this.setState({
-      [name]: value,
-    })
-  }
 
   handleSubmit = event => {
-    event.preventDefault()
-    alert(`Welcome ${this.state.fullName} ${this.state.email}!`)
+    emailjs.sendForm('contact_service','contact_form', '#contact-form', 'user_RfSf1tcSTDweZVWXo3B3m')
+        .then((response) => {
+           console.log('SUCCESS!', response.status, response.text);
+        }, (err) => {
+           console.log('FAILED...', err);
+        })
+    alert(`Your message has been sent`)
   }
 
   render() {
     return (
       <Layout>
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Full name
-          <input
-            type="text"
-            name="fullName"
-            value={this.state.fullName}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <label>
-          Last name
-          <input
-            type="text"
-            name="email"
-            value={this.state.email}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <label>
-          Message
-          <input
-            type="text"
-            name="message"
-            value={this.state.message}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <button type="submit">Submit</button>
+        <form id="contact-form" onSubmit={this.handleSubmit}>
+          <input type="hidden" name="contact_number" />
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="text"></textarea>
+          <input type="submit" value="Send" />
       </form>
     </Layout>
     )
