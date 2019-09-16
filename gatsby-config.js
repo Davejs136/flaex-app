0fa43eeb96f66f900af8252b27a4f7ed2ac13674
Freddy Polania <fpolania@gmail.com>
2019-09-16 13:46:01 -0400

    Updates
    - APIurl config
    - gitignore

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,50 +1,53 @@
 module.exports = {
   siteMetadata: {
-    title: '@flaex_ portfolio and blog',
-    description: 'Gatsby portfolio and blog with Strapi',
-    author: 'Freddy Polania'
+    title: "@flaex_ portfolio and blog",
+    description: "Gatsby portfolio and blog with Strapi",
+    author: "Freddy Polania",
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
-      }
+      },
     },
     {
-      resolve: 'gatsby-source-strapi',
+      resolve: "gatsby-source-strapi",
       options: {
-        apiURL: 'http://localhost:1337',
-        contentTypes: [ // List of the Content Types you want to be able to request from Gatsby.
-          'profile',
-          'location',
-          'skill',
-          'link',
-          'article',
-          'work',
-          'user',
-          'position'
+        apiURL: process.env.DEPLOY_URL
+          ? "https://flaex-app.herokuapp.com"
+          : "http://localhost:1337",
+        contentTypes: [
+          // List of the Content Types you want to be able to request from Gatsby.
+          "profile",
+          "location",
+          "skill",
+          "link",
+          "article",
+          "work",
+          "user",
+          "position",
         ],
         queryLimit: 1000,
       },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
+        name: "gatsby-starter-default",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "src/images/favicon.png", // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-offline',
-    `gatsby-plugin-less`
+    "gatsby-plugin-offline",
+    `gatsby-plugin-less`,
   ],
 }
