097b65b69a64821afc11c17724cc21c52ab7bb84
Freddy Polania <fpolania@gmail.com>
2019-09-19 18:53:07 -0400

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@ import Navigation from "./navigation"
 import Footer from "./footer"
 
 const Layout = ({ children }) => (
-  <div className="home">
+  <div className="page">
     <div className="menu">
       <HeaderPage />
       <Navigation />
