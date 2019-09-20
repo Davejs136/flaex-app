9f59afff85ed37677f220d4a37bee869f31f6cf6
Freddy Polania <fpolania@gruponovanet.com.ve>
2019-09-19 22:46:08 -0400

    Update page.js

--- src/pages/page.js
+++ src/pages/page.js
@@ -2,9 +2,10 @@ import React from "react"
 import PropTypes from "prop-types"
 import Layout from "../components/layout"
 import { library } from "@fortawesome/fontawesome-svg-core"
+import { fab } from "@fortawesome/free-brands-svg-icons"
 import { fas } from "@fortawesome/free-solid-svg-icons"
 
-library.add(fas)
+library.add(fab, fas)
 
 const Page = ({ children }) => (
   <Layout>
