de568a4f2ddcba82bb05f127bcd4ce43a02afa35
Freddy Polania <fpolania@gmail.com>
2019-10-01 23:08:04 -0400

    Fullscreen test

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,7 @@ module.exports = {
         short_name: "flaex",
         start_url: "/",
         background_color: "#fff",
-        theme_color: "#000",
+        theme_color: "#fff",
         display: "standalone",
         icon: "src/images/favicon.png", // This path is relative to the root of the site.
       },
