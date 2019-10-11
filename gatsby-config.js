1768e962dbc56aa1d2196e2b6df69b4349b465b6
Freddy Polania <fpolania@gmail.com>
2019-10-11 14:49:23 -0400

    sb change

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     titleTemplate: "%s · The Real Hero",
     description:
       "Hogwarts Potions master, Head of Slytherin house and former Death Eater.",
-    url: "https://www.doe.com", // No trailing slash allowed!
+    siteUrl: "https://www.doe.com", // No trailing slash allowed!
     image: "/images/snape.jpg", // Path to your image you placed in the 'static' folder
     twitterUsername: "@occlumency",
   },
