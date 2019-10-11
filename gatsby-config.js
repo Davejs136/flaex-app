8ec692d676715dcae0622dad227bd1b828b5517f
Freddy Polania <fpolania@gmail.com>
2019-10-11 14:54:32 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: "Severus Snape",
-    titleTemplate: "%s · The Real Hero",
-    description:
-      "Hogwarts Potions master, Head of Slytherin house and former Death Eater.",
-    siteUrl: "https://www.doe.com", // No trailing slash allowed!
-    image: "/images/snape.jpg", // Path to your image you placed in the 'static' folder
-    twitterUsername: "@occlumency",
+   
+    title: "Diseñador gráfico & desarrollador front-end",
+    titleTemplate: "%s · @flaex_",
+    description: "Portafolio & blog | Freddy Polanía",
+    keywords: "Diseño gráfico desarrollo fron-end pwa",
+    author: "Freddy Polania",
+    siteUrl: "https://flaex.netlify.com",
+    image: "/seo-img.jpg", 
+    twitterUsername: "@Flaex_",
   },
   plugins: [
     {
