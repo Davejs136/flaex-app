368bd8a6dfb97a12ee7ef9124ce31ece6c190088
Freddy Polania <fpolania@gmail.com>
2019-10-18 12:45:19 -0400

    Update googleMap.js

--- src/components/googleMap.js
+++ src/components/googleMap.js
@@ -12,7 +12,7 @@ const defaultProps = {
 const GoogleMap = () => (
   <div className="map">
     <GoogleMapReact
-      bootstrapURLKeys={{ key: process.env.GOOGLE_MAPS_API_KEY }}
+      bootstrapURLKeys={{ key: `${process.env.GATSBY_GOOGLE_MAPS_API_KEY}` }}
       defaultCenter={defaultProps.center}
       defaultZoom={defaultProps.zoom}
     />
